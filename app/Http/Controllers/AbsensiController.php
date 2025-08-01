8c9c45253e6903024d03233483efaf3887ade1c1
Sendi2144T <sendi2144t@gmail.com>
2025-08-01 10:14:04 +0700

    sendi

--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -1,31 +1,10 @@
-// app/Http/Controllers/AbsensiController.php
+<?php
 
-use App\Models\Absensi;
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
 class AbsensiController extends Controller
 {
-    public function index()
-    {
-        $absensis = Absensi::all();
-        return view('absensi.index', compact('absensis'));
-    }
-
-    public function create()
-    {
-        return view('absensi.create');
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'nama' => 'required',
-            'tanggal' => 'required|date',
-            'status' => 'required|in:Hadir,Izin,Sakit,Alpha',
-        ]);
-
-        Absensi::create($request->all());
-
-        return redirect()->route('absensi.index')->with('success', 'Data berhasil ditambahkan');
-    }
+    //
 }
